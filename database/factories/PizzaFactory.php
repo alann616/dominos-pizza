f57f7cf305913edc32b49ce97304a23c011cbb8f
alann616 <morningstar.alan616@gmail.com>
2024-10-26 10:20:55 -0600

    Upload proyect

---
+++ database/factories/PizzaFactory.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pizza>
+ */
+class PizzaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $toppingChoices = [
+            'extra cheese',
+            'black olives',
+            'pepperoni',
+            'sausage',
+            'anchovies',
+            'jalapenos',
+            'onion',
+            'chicken',
+            'ground beef',
+            'green peppers',
+        ];
+
+        $toppings = [];
+
+        for($i = 1; $i <= rand(1, 4); $i++) {
+            $toppings[] = Arr::random($toppingChoices);
+        }
+
+        $toppings = array_unique($toppings);
+
+        return [
+            'id' => round(11111, 99999),
+            'user_id' => round(1, 10),
+            'size' => Arr::random(['small', 'medium', 'large', 'extra large']),
+            'crust' => Arr::random(['normal', 'thin', 'garlic']),
+            'toppings' => $toppings,
+            'status' => Arr::random(['ordered', 'prepping', 'baking', 'checking', 'ready']),
+        ];
+    }
+}
